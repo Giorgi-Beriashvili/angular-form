7d194e6674ca8e864a7832f10b17d6c44f1e6904
Sundance Kid <maverick891012@gmail.com>
2022-12-05 03:45:04 +0400

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,107 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'form-app';
+    userForm: FormGroup = new FormGroup({
+        firstname: new FormControl('', [
+            Validators.required,
+            Validators.maxLength(10),
+            Validators.minLength(2),
+        ]),
+        lastname: new FormControl('', [
+            Validators.required,
+            Validators.pattern('^[a-zA-Z]+$'),
+        ]),
+        email: new FormControl('', [Validators.email, Validators.required]),
+        phone: new FormControl('', [
+            Validators.required,
+            Validators.minLength(9),
+            Validators.maxLength(13),
+        ]),
+        github: new FormControl('', [
+            Validators.required,
+            Validators.pattern(
+                '^(http(s?)://)?(www.)?github.([a-z])+/([A-Za-z0-9]{1,})+/?$'
+            ),
+        ]),
+        linkedin: new FormControl('', [
+            Validators.required,
+            Validators.pattern(
+                '^(http(s?)://)?(www.)?linkedin.([a-z])+/([A-Za-z0-9]{1,})+/?$'
+            ),
+        ]),
+
+        experience: new FormGroup({
+            work: new FormControl('', [
+                Validators.required,
+                Validators.minLength(4),
+                Validators.maxLength(4),
+            ]),
+            from: new FormControl('', [Validators.required]),
+            to: new FormControl('', [Validators.required]),
+        }),
+
+        education: new FormGroup({
+            institution: new FormControl('', [Validators.required]),
+            from: new FormControl('', [Validators.required]),
+            to: new FormControl('', [Validators.required]),
+        }),
+    });
+
+    get firstname() {
+        return this.userForm.get('firstname');
+    }
+
+    get lastname() {
+        return this.userForm.get('lastname');
+    }
+
+    get email() {
+        return this.userForm.get('email');
+    }
+
+    get phone() {
+        return this.userForm.get('phone');
+    }
+
+    get github() {
+        return this.userForm.get('github');
+    }
+
+    get linkedin() {
+        return this.userForm.get('linkedin');
+    }
+
+    get work() {
+        return this.userForm.get('experience')?.get('work');
+    }
+
+    get workFrom() {
+        return this.userForm.get('experience')?.get('from');
+    }
+
+    get workTo() {
+        return this.userForm.get('experience')?.get('to');
+    }
+
+    get institution() {
+        return this.userForm.get('education')?.get('institution');
+    }
+
+    get institutionFrom() {
+        return this.userForm.get('education')?.get('from');
+    }
+
+    get institutionTo() {
+        return this.userForm.get('education')?.get('to');
+    }
+
+    onSubmit() {
+        console.log(this.userForm);
+    }
 }
